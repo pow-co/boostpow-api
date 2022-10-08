41b43c530c05795f0f9afdcd03f967e30eaf9023
Zyler <508282+zylerdj@users.noreply.github.com>
2022-10-08 10:46:59 +0200

    whatsonchain api methods for blocks

--- src/test/lib/whatsonchain_test.ts
+++ src/test/lib/whatsonchain_test.ts
@@ -1,15 +1,50 @@
-import { getTransaction } from '../../whatsonchain'
+import * as whatsonchain from '../../whatsonchain'
 import { expect } from '../utils'
 
 describe("Whatsonchain API", () => {
 
     it("#getTransaction should return tx hex given a txid", async () => {
 
-        const result = await getTransaction('8db59c48e77614e7db5c73a95b0705f312dd88aa2c2f83d71bffd931926d29e9')
+        const result = await whatsonchain.getTransaction('8db59c48e77614e7db5c73a95b0705f312dd88aa2c2f83d71bffd931926d29e9')
 
         console.log(result)
 
         expect(result)
 
+    })
+
+    it('getBlockByHash should get a block by hash', async () => {
+
+        const hash = '000000000000000004a288072ebb35e37233f419918f9783d499979cb6ac33eb'
+
+        const result = await whatsonchain.getBlockByHash({ hash })
+
+        expect(result.hash).to.be.equal(hash)
+
+    })
+
+    it("getBlockByHeight should get the block by height", async () => {
+
+        const height = 700_000
+
+        const result = await whatsonchain.getBlockByHeight({ height })
+
+        expect(result.height).to.be.equal(height)
+
+    })
+
+    it("getBlockPages should transactions in a block with more than 100", async () => {
+
+
+
+        const hash = '000000000000000000885a4d8e9912f085b42288adc58b3ee5830a7da9f4fef4'
+
+        const result = await whatsonchain.getBlockPages({
+            hash,
+            number: 1
+        })
+
+        expect(result[0]).to.be.equal('51c4933d986da4c0de51ea8446b7db4aa1753f205c594591a09998b1d05d7cfe')
+        
     })
 })
--- src/whatsonchain.ts
+++ src/whatsonchain.ts
@@ -1,5 +1,6 @@
 
-import * as http from 'superagent'
+import axios from 'axios'
+import { ExecSyncOptionsWithStringEncoding } from 'child_process';
 
 interface WhatsonchainTransaction {
   txid: string;
@@ -15,9 +16,102 @@ export async function getTransaction(txid: string): Promise<WhatsonchainTransact
 
   let url =`https://api.whatsonchain.com/v1/bsv/main/tx/hash/${txid}`
 
-  let {body} = await http.get(url)
+  let {data} = await axios.get(url)
 
-  return body
+  return data
 
 }
 
+interface Block {
+  hash: string;
+  confirmations: number;
+  size: number;
+  height: number;
+  version: number;
+  versionHex: string;
+  merkelroot: string;
+  txcount: number;
+  tx: string[];
+  time: number;
+  mediantime: number;
+  nonce: number;
+  bits: string;
+  difficulty: number;
+  chainwork: string;
+  previousblockhash: string;
+  nextblockhash: string;
+  coinbaseTx: {
+    hex: string;
+    txid: string;
+    hash: string;
+    size: number;
+    version: number;
+    locktime: number;
+    vin: {
+      txid: string;
+      vout: number;
+      scriptSig: {
+        asm: string;
+        hex: string;
+      };
+      sequence: number;
+      coinbase: string; 
+    }[];
+    vout: {
+      value: number;
+      n: number;
+      scriptPubKey: {
+        asm: string;
+        hex: string;
+        reqSigs: number;
+        type: number;
+        addresses: string[];
+        opReturn: string;
+      }
+    }[];
+    blockhash: string;
+    confirmations: number;
+    time: number;
+    blocktime: number;
+  }
+  totalFees: number;
+  miner: string;
+  pages: any;
+}
+
+
+export async function getBlockByHeight({ height }: { height: number }): Promise<Block> {
+
+  let url = `https://api.whatsonchain.com/v1/bsv/main/block/height/${height}`
+
+  let {data} = await axios.get(url)
+
+  return data
+
+}
+
+
+export async function getBlockByHash({ hash }): Promise<Block> {
+
+  let url =`https://api.whatsonchain.com/v1/bsv/main/block/hash/${hash}`
+
+  let {data} = await axios.get(url)
+
+  return data
+
+}
+
+interface GetBlockPages {
+  number: number;
+  hash: string;
+}
+
+export async function getBlockPages({ hash, number }: GetBlockPages): Promise<WhatsonchainTransaction> {
+
+  let url = `https://api.whatsonchain.com/v1/bsv/main/block/hash/${hash}/page/${number}`
+
+  let response = await axios.get(url)
+
+  return response.data
+
+}
