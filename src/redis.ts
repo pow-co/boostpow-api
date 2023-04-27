66f98b2f7eb8b610a30086b8de67fa1fbca78d7b
Katrina Knight <kat.knight@newgaea.net>
2023-04-27 03:29:45 +0100

    feat: minor change to fix redis connection

--- src/redis.ts
+++ src/redis.ts
@@ -1,15 +1,27 @@
 
 import { createClient } from 'redis';
 
-let client = createClient()
+let client;
 
 if (process.env.REDIS_URL) {
 
-  client = createClient({ url: process.env.REDIS_URL })
+  client = createClient({ url: process.env.REDIS_URL,
+  socket: {
+      connectTimeout: 50000,
+    } })
 
 }
+else {
+  client = createClient({
+    socket: {
+      connectTimeout: 50000,
+    }
+  });
+}
 
-client.connect()
-
+const start = async ()=> {
+await client.connect()
+}
+start();
 export default client
 
